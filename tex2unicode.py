beeaaf73b6b6e5a2562c0c384f506c6004c824da
Daniel Thau <danthau@gmail.com>
2011-02-26 12:06:22 -0500

    Square and CheckedBox

--- tex2unicode.py
+++ tex2unicode.py
@@ -179,6 +179,8 @@ def command_run_input(data, buffer, command):
         weechatbuffer=unicode(weechatbuffer.replace('\\dots',u'\u2026'))
         weechatbuffer=unicode(weechatbuffer.replace('\\ldots',u'\u2026'))
         weechatbuffer=unicode(weechatbuffer.replace('\\Telephone',u'\u260F'))
+        weechatbuffer=unicode(weechatbuffer.replace('\\Square',u'\u2610'))
+        weechatbuffer=unicode(weechatbuffer.replace('\\CheckedBox',u'\u2611'))
         weechatbuffer=unicode(weechatbuffer.replace('\\frac{1}{2}',u'\u00BD'))
         weechatbuffer=unicode(weechatbuffer.replace('\\frac{1}{4}',u'\u00BC'))
         weechatbuffer=unicode(weechatbuffer.replace('\\frac{3}{4}',u'\u00BE'))
